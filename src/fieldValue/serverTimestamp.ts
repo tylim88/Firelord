8f5cec28613d7b2b9480979a4006696d1d504798
tylim <limteikyee@gmail.com>
2022-08-05 06:39:11 +0800

    add server timestamp unit test

--- src/fieldValue/serverTimestamp.ts
+++ src/fieldValue/serverTimestamp.ts
@@ -5,6 +5,5 @@ import { ServerTimestamp } from '../types'
 Returns a sentinel used with @firebase/firestore/lite#(setDoc:1) or @firebase/firestore/lite#(updateDoc:1) to include a server-generated timestamp in the written data.
  */
 export const serverTimestamp = () => {
-	const ref = FieldValue.serverTimestamp() as ServerTimestamp
-	return ref
+	return FieldValue.serverTimestamp() as ServerTimestamp
 }
