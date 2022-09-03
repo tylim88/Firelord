0169b90c5b00de4835fc14d0655359423b551b7e
Acid Coder <5227509+tylim88@users.noreply.github.com>
2022-09-04 05:09:36 +0800

    Update init.ts

--- codeForDoc/src/init.ts
+++ codeForDoc/src/init.ts
@@ -5,17 +5,17 @@ import { Example } from './dataType'
 const app = initializeApp()
 
 // if you run in custom backend or you run in cloud function but want to access other project firestore
-import serviceAccount from './serviceAccount.json' // get it from firebase console --> project setting --> service accounts, THIS IS A SECRET!
-const app_ = initializeApp({
+import serviceAccount from './serviceAccount.json' // get it from firebase console --> project setting --> service accounts, THIS IS A SECERT!
+const app = initializeApp({
 	credential: cert(serviceAccount as ServiceAccount),
 })
 
-export const db = getFirestore()
+export const db = getFirestore() // or getFirestore(app), you can skip this line
 
 // Recommendation: Export this if the collection is sub-collection and then fill in collection path later, because sub collection most likely have dynamic document ID.
-export const firelordExample = getFirelord<Example>(db) // shorthand: getFirelord<Example>()
+export const firelordExample = getFirelord<Example>() // or getFirelord<Example>(db)
 // then call them using
-// const example = firelordExample(`parentCollectionName/${someDocId}/SomeCollectionName`)
+// const example = firelordExample(`parentCollectionName/${someDocId}/SomeCollectionName`) // this is your firelordRef
 
 // Recommendation: Export this if the collection is root collection
-export const example = firelordExample('SomeCollectionName')
+export const example = firelordExample('SomeCollectionName') // this is your firelordRef
