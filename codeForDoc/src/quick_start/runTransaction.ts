3dcaecdedc2e1dee2e223d2bcb567614223eb53f
tylim <limteikyee@gmail.com>
2022-09-08 22:39:05 +0800

    - add test for transaction and update code for doc - replace parcel with rollup

--- codeForDoc/src/quick_start/runTransaction.ts
+++ codeForDoc/src/quick_start/runTransaction.ts
@@ -4,39 +4,46 @@ import {
 	serverTimestamp,
 	increment,
 	arrayRemove,
+	Timestamp,
 } from 'firelord'
 
 export const dummy = async () => {
 	try {
-		await runTransaction(db, async transaction => {
-			// ...
-		})
 		// OR you can skip 'db'
-		await runTransaction(async transaction => {
-			await transaction.get(example.doc('lmn')).then(docSnapshot => {
-				const data = docSnapshot.data()
-			})
+		await runTransaction(
+			db,
+			async transaction => {
+				await transaction.get(example.doc('lmn')).then(docSnapshot => {
+					const data = docSnapshot.data()
+				})
 
-			transaction.create(example.doc('lmn'), {
-				a: 500,
-				b: { c: true, d: [{ e: 'efg' }] },
-				f: { g: serverTimestamp(), h: 3838 },
-			})
+				transaction.create(example.doc('lmn'), {
+					a: 500,
+					b: { c: true, d: [{ e: 'efg' }] },
+					f: { g: serverTimestamp(), h: 3838 },
+				})
 
-			transaction.set(example.doc('lmn'), {
-				a: 88,
-				b: { c: false, d: [{ e: 'opq' }] },
-				f: { g: serverTimestamp(), h: 2929 },
-			})
+				transaction.set(example.doc('lmn'), {
+					a: 88,
+					b: { c: false, d: [{ e: 'opq' }] },
+					f: { g: serverTimestamp(), h: 2929 },
+				})
 
-			transaction.update(example.doc('lmn'), {
-				a: increment(1),
-				b: { d: arrayRemove({ e: 'rst' }) }, // nested form
-				'f.g': serverTimestamp(), // dot notation form
-			})
+				transaction.update(example.doc('lmn'), {
+					a: increment(1),
+					b: { d: arrayRemove({ e: 'rst' }) }, // nested form
+					'f.g': serverTimestamp(), // dot notation form
+				})
 
-			transaction.delete(example.doc('lmn'))
-		})
+				transaction.delete(example.doc('lmn'))
+			},
+			{ readOnly: true, readTime: Timestamp.now() } // optional
+			// we can supply 2 set of options:
+			// 1. { readOnly?: false, maxAttempts?: number }
+			// note that in case 1 both properties are optional and readOnly is false
+			// 2. { readOnly: true, readTime?: Timestamp }
+			// note that in case only readTime is optional and readOnly is true
+		)
 		console.log('Transaction successfully committed!')
 	} catch (e) {
 		console.log('Transaction failed: ', e)
--- src/runTransaction/set.test.ts
+++ src/runTransaction/set.test.ts
@@ -9,7 +9,10 @@ import { runTransaction } from '.'
 import { setDoc, getDoc } from '../operations'
 import { TransactionSet, IsTrue, IsSame } from '../types'
 import { setCreator } from './set'
+import { getFirestore, Timestamp } from 'firebase-admin/firestore'
+
 initializeApp()
+const db = getFirestore()
 const userRef = userRefCreator()
 describe('test set transaction', () => {
 	it('test whether the return type is correct', () => {
@@ -26,14 +29,14 @@ describe('test set transaction', () => {
 			IsTrue<IsSame<typeof A, 1>>()
 		}
 	})
-	it('test set functionality', async () => {
+	it('test set functionality, with db', async () => {
 		const docRef = userRef.doc('setTransactionTestCase')
 		const docRef2 = userRef.doc('setTransactionTestCase2')
 		const docRef3 = userRef.doc('setTransactionTestCase3')
 		const data = generateRandomData()
 		const data2 = generateRandomData()
 		const data3 = generateRandomData()
-		await runTransaction(async transaction => {
+		await runTransaction(db, async transaction => {
 			transaction.set(docRef, data)
 			transaction.set(docRef2, data2)
 			transaction.set(docRef3, data3)
@@ -42,26 +45,33 @@ describe('test set transaction', () => {
 		await readThenCompareWithWriteData(data2, docRef2)
 		await readThenCompareWithWriteData(data3, docRef3)
 	})
-	it('test read functionality', async () => {
+	it('test read functionality, with options', async () => {
 		const docRef = userRef.doc('setTransactionTestCaseRead')
 		const data = generateRandomData()
 		await setDoc(docRef, data)
-		await runTransaction(async transaction => {
-			const docSnap = await transaction.get(docRef)
-			compareWriteDataWithDocSnapData(data, docSnap)
-		})
+		await runTransaction(
+			async transaction => {
+				const docSnap = await transaction.get(docRef)
+				compareWriteDataWithDocSnapData(data, docSnap)
+			},
+			{ readOnly: true, readTime: Timestamp.now() }
+		)
 	})
 	it('test delete functionality', async () => {
 		const docRef = userRef.doc('setTransactionTestCaseRead')
 		const data = generateRandomData()
 		await setDoc(docRef, data)
-		await runTransaction(async transaction => {
-			transaction.delete(docRef)
-		})
+		await runTransaction(
+			db,
+			async transaction => {
+				transaction.delete(docRef)
+			},
+			{ readOnly: false, maxAttempts: 6 }
+		)
 		const docSnap = await getDoc(docRef)
 		expect(docSnap.exists).toBe(false)
 	})
-	it('test merge true functionality', async () => {
+	it('test merge true functionality, with db and options', async () => {
 		const ref = userRef.doc('setTransactionTestMergeCase')
 		const data = generateRandomData()
 		await setDoc(ref, data)
