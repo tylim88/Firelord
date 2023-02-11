07090b6f9b13e947df377490d9c7f2d7e5e2c26a
tylim <limteikyee@gmail.com>
2023-02-12 00:54:58 +0800

    housekeeping

--- codeForDoc/src/quick_start/runTransaction.ts
+++ codeForDoc/src/quick_start/runTransaction.ts
@@ -9,8 +9,8 @@ import {
 //
 ;async () => {
 	try {
-		await runTransaction(
-			db,
+		const result = await runTransaction(
+			db, // db argument is optional, you can skip it
 			async transaction => {
 				await transaction.get(example.doc('lmn')).then(docSnapshot => {
 					const data = docSnapshot.data()
@@ -30,15 +30,17 @@ import {
 
 				transaction.update(example.doc('lmn'), {
 					a: increment(1),
-					b: { d: arrayRemove({ e: 'rst' }) },
-					'f.g': serverTimestamp(),
+					b: { d: arrayRemove({ e: 'rst' }) }, // nested form
+					'f.g': serverTimestamp(), // dot notation form
 				})
 
 				transaction.delete(example.doc('lmn'))
+
+				return 123 // return this to result
 			},
-			{ readOnly: true, readTime: Timestamp.now() }
+			{ readOnly: true, readTime: Timestamp.now() } // optional, { readOnly?: false, maxAttempts?: number } OR { readOnly: true, readTime?: Timestamp }
 		)
-		console.log('Transaction successfully committed!')
+		console.log(result) // result is 123 because we return 123 in runTransaction callback
 	} catch (e) {
 		console.log('Transaction failed: ', e)
 	}
