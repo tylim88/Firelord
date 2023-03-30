7b70cdb1795c045be96d270e6613ad15c7bfe0d2
tylim <limteikyee@gmail.com>
2023-03-30 17:19:59 +0800

    simplify query type

--- src/types/query.ts
+++ src/types/query.ts
@@ -5,21 +5,15 @@ import {
 	AddSentinelFieldPathToCompareHighLevel,
 } from './fieldPath'
 import { QueryConstraintLimitation } from './queryConstraintLimitation'
-import { IsEqual } from './utils'
 import { Query, CollectionReference } from './refs'
 
 export type QueryRef = <
 	T extends MetaType,
-	Q extends Query<T>,
+	Q extends Query<T> | CollectionReference<T>,
 	QC extends QueryConstraints<AddSentinelFieldPathToCompare<T>>[]
 >(
-	query: Q extends never
-		? Q
-		: IsEqual<Q, Query<T>> extends true
-		? Query<T>
-		: IsEqual<Q, CollectionReference<T>> extends true
-		? CollectionReference<T>
-		: never, // has to code this way to infer T perfectly
+	query: Q extends never ? Q : Query<T> | CollectionReference<T>,
+
 	...queryConstraints: QC extends never
 		? QC
 		: QueryConstraintLimitation<
