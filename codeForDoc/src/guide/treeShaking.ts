85880981f963096b2f8ea14b2dba02241ca40404
tylim <limteikyee@gmail.com>
2023-06-08 18:07:24 +0800

    test ci

--- codeForDoc/src/guide/treeShaking.ts
+++ codeForDoc/src/guide/treeShaking.ts
@@ -1,35 +1,36 @@
-import {
-	getFirelord,
-	getFirestore,
-	MetaTypeCreator,
-	docCreator,
-	collectionCreator,
-	collectionGroupCreator,
-} from 'firelord'
-type ExampleParent = MetaTypeCreator<
-	{
-		b: number
-	},
-	'someCollectionId_1'
->
-type Example = MetaTypeCreator<
-	{
-		a: number
-	},
-	'someCollectionId_2',
-	string,
-	ExampleParent
->
-const db = getFirestore()
-const collectionID = 'someCollectionId_2'
-const collectionIDArr = ['someCollectionId_1', collectionID] as const // it is an array
+// import {
+// 	getFirelord,
+// 	getFirestore,
+// 	MetaTypeCreator,
+// 	docCreator,
+// 	collectionCreator,
+// 	collectionGroupCreator,
+// } from 'firelord'
+// type ExampleParent = MetaTypeCreator<
+// 	{
+// 		b: number
+// 	},
+// 	'someCollectionId_1'
+// >
+// type Example = MetaTypeCreator<
+// 	{
+// 		a: number
+// 	},
+// 	'someCollectionId_2',
+// 	string,
+// 	ExampleParent
+// >
+// const db = getFirestore()
+// const collectionID = 'someCollectionId_2'
+// const collectionIDArr = ['someCollectionId_1', collectionID] as const // it is an array
 
-const example1 = getFirelord<Example>(db, ...collectionIDArr)
+// const example1 = getFirelord<Example>(db, ...collectionIDArr)
 
-// example2 and example1 are the same thing
-// example2 is tree shakable because you can remove unwanted functionalities.
-const example2 = {
-	doc: docCreator<Example>(db, ...collectionIDArr),
-	collection: collectionCreator<Example>(db, ...collectionIDArr),
-	collectionGroup: collectionGroupCreator<Example>(db, collectionID), // collection group requires only the last segment collection id
-}
+// // example2 and example1 are the same thing
+// // example2 is tree shakable because you can remove unwanted functionalities.
+// const example2 = {
+// 	doc: docCreator<Example>(db, ...collectionIDArr),
+// 	collection: collectionCreator<Example>(db, ...collectionIDArr),
+// 	collectionGroup: collectionGroupCreator<Example>(db, collectionID), // collection group requires only the last segment collection id
+// }
+export const a = 1
