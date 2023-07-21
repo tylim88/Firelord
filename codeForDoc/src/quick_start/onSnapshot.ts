5daa9b37ab51e850efbe09a815e6bfa04bf73736
tylim <limteikyee@gmail.com>
2023-07-21 14:43:03 +0800

    housekeeping

--- codeForDoc/src/quick_start/onSnapshot.ts
+++ codeForDoc/src/quick_start/onSnapshot.ts
@@ -7,24 +7,39 @@ const unsub = onSnapshot(
 		example.collectionGroup(),
 		where('b.d', 'array-contains', { e: 'hello' }),
 		orderBy('f.g'),
-		startAfter(new Date()),
-		offset(1)
+		startAfter(new Date())
 	),
 	querySnapshot => {
 		// return querySnapshot similar to querySnapshot of getDocs
 	},
-	error => {}
+	error => {
+		// handle error
+	}
+)
+
+// listen to entire collection
+const unsub2 = onSnapshot(
+	example.collectionGroup(),
+	querySnapshot => {
+		// return querySnapshot similar to querySnapshot of getDocs
+	},
+	error => {
+		// handle error
+	}
 )
 
 // listen to a single document
-const unsub2 = onSnapshot(
+const unsub3 = onSnapshot(
 	example.doc('abc'),
 	docSnapshot => {
 		// return docSnapshot similar to docSnapshot of getDoc
 	},
-	error => {}
+	error => {
+		// handle error
+	}
 )
 
 // remove listeners
 unsub()
 unsub2()
+unsub3()
